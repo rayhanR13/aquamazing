14705779bf9400a779c3fc93e39a1be41572234f
Rayhan Mahmud <rayhan1550@gmail.com>
2021-01-07 13:10:53 +0600

    modal added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,36 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
-const navLi = document.querySelectorAll('.nav-link li');
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const navLi = document.querySelectorAll(".nav-link li");
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('open');
-    navLi.forEach(li => {
-        li.classList.toggle('fade');
+const modalContainer = document.querySelector(".modal-container");
+const modalFullImg = document.querySelector(".modal-full-img");
+const modalImages = document.querySelectorAll(".article-img");
+
+// Responsive hamburger acion
+burger.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    navLi.forEach((li) => {
+        li.classList.toggle("fade");
     });
+});
+
+// Modal action
+modalImages.forEach((image) => {
+    image.addEventListener("click", () => {
+        modalContainer.classList.add("open");
+        modalFullImg.classList.add("open");
+
+        const imgSrc = image.getAttribute("src");
+        modalFullImg.src = imgSrc;
+
+        console.log(modalFullImg);
+        console.log(imgSrc);
+    });
+});
+
+modalContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal-container")) {
+        modalContainer.classList.remove("open");
+        modalFullImg.classList.remove("open");
+    }
 });
